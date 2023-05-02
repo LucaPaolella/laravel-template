951cc233f50cd3e426149cbe85e36400f1687f3b
Your Name <you@example.com>
2023-05-02 17:32:58 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+import './bootstrap';
+import '~resources/scss/app.scss'
+
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// gestione immagini build
+import.meta.glob([
+    '../img/**'
+])
+'~resources/scss/app.scss'
+
+/*import.meta.glob([
+    '../img/**'
+]);*/
